8f86e358d0c230b8a9b7b2c1727046894c8359d0
Tony <bobacrypto2@gmail.com>
2022-06-20 03:27:26 -0700

    create ui and dom for ui (not done)

--- script.js
+++ script.js
@@ -3,8 +3,38 @@ let playerSelection;
 let selection = ["rock", "paper", "scissor", "scissors"];
 let rounds = 0;
 
+// Create buttons and appends to HTML (page) body
+const rockBtn = document.createElement('button');
+const paperBtn = document.createElement('button');
+const scissorBtn = document.createElement('button');
+const body = document.querySelector('body');
+const optionDiv = document.createElement('div');
+const displayContainer = document.createElement('div');
+const display = document.createElement('div');
 
+rockBtn.textContent = 'Rock';
+paperBtn.textContent = 'Paper';
+scissorBtn.textContent = 'Scissors';
 
+optionDiv.classList.add('options');
+displayContainer.classList.add('score');
+display.classList.add('score-info');
+
+//Appending created elements
+body.append(displayContainer, optionDiv);
+displayContainer.append(display);
+optionDiv.append(rockBtn, paperBtn, scissorBtn);
+
+// Target buttons to add Events
+const button = document.querySelectorAll('button');
+const selectDisplay = document.querySelector('.score-info');
+
+button.forEach(btn => btn.addEventListener('click', () => {
+    playerSelection = btn.innerText;
+    game();
+}));
+
+// Gameplay Functions
 function computerPlay() {
     let randomNum = Math.floor(Math.random()*3);
     computerSelection = selection[randomNum];
@@ -24,19 +54,19 @@ function play(player, cpu) {
             player == "rock" && cpu == "paper" ||
             player == "paper" && cpu == "scissor") {
 
-            console.log(`${cpu} beats ${player}! You lose.`);
-            console.log("Rounds:" + ++rounds);
+            selectDisplay.textContent = `${cpu} beats ${player}! You lose.`;
+            selectDisplay.textContent += "Rounds:" + ++rounds;
 
         } else if (player == "scissor" && cpu == "paper" ||
                     player == "rock" && cpu == "scissor" ||
                     player == "paper" && cpu == "rock") {
 
-            console.log(`${player} beats ${cpu} You win!`);
-            console.log("Rounds:" + ++rounds);
+            selectDisplay.textContent = `${player} beats ${cpu} You win!`;
+            selectDisplay.textContent += "Rounds:" + ++rounds;
 
         } else {
-            console.log(`${player} and ${cpu} results in a tie!`);
-            console.log("Rounds:" + ++rounds);
+            selectDisplay.textContent = `${player} and ${cpu} results in a tie!`;
+            selectDisplay.textContent = "Rounds:" + ++rounds;
         }
 }
 
@@ -45,10 +75,11 @@ function gameReset() {
 }
 
 // Account for Cancel or Empty inputs on prompt
+//------------------------------------------------
 function caseCheck(player) {
     if (player == null || player == undefined || !(selection.includes(player.toLowerCase()))) {
-        console.log("Please enter an valid option!");
-        console.log(`You entered: ${player}`);
+        selectDisplay.textContent = "Please enter an valid option!";
+        selectDisplay.textContent = `You entered: ${player}`;
         check = false;
     } else {
         check = true;
@@ -58,17 +89,24 @@ function caseCheck(player) {
 // Game start function to execute everything required to start a fresh game.
 function game() {
 
-    gameReset();
+    if (rounds == 5) {
+        selectDisplay.textContent = 'Game is over! 5 Rounds have been played out.';
+        gameReset();
+    }
 
-    for (i = 0; i < 5; i++) {
-    playerSelection = prompt("Please choose either Rock, Paper, or Scissors");
     computerPlay();
+
+    // Check for error options and case sensitivity
+    // ------------------------------------------------
+    // for (i = 0; i < 5; i++) {
     caseCheck(playerSelection);
         if (check == true) {
             play(playerSelection, computerSelection);
-        } else {
-            break;
         }
-    }
+    //      else {
+    //         break;
+    //     }
+    // }
 
 }
+
